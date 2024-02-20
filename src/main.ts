605ea031d04c9040c9c06dea6a49ff780868c3b9
ttp12345 <ttp12345@thirautech.com>
2024-02-20 10:26:05 +0900

    edit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     AppModule, 
     {abortOnError: false} // 기본적으로 애플리케이션을 생성하는 동안 오류가 발생하면 앱은 코드와 함께 종료됩니다. 오류가 발생하도록 하려면 옵션 abortOnError을 비활성화
   );
-  await app.listen(3000);
+  await app.listen(8082);
 }
 bootstrap();
