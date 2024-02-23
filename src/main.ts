5d1c2d75dc99525e781f52cfdb323175d010ef99
ttp12345 <ttp12345@thirautech.com>
2024-02-23 14:25:33 +0900

    add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule, 
     {abortOnError: false} // 기본적으로 애플리케이션을 생성하는 동안 오류가 발생하면 앱은 코드와 함께 종료됩니다. 오류가 발생하도록 하려면 옵션 abortOnError을 비활성화
   );
+  await app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: true, // 자세한 오류 비활성화
+  }));
   await app.listen(8082);
 }
 bootstrap();
